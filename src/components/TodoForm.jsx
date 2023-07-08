1ffa8f01f56866f9e99306f01c827878a2fd04eb
Shota Pailodze <55694002+shotapailodze@users.noreply.github.com>
2023-07-08 14:55:02 +0400

    functionality done

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
 const [input, setInput] = useState('');
+
+const inputRef = useRef(null)
+
+useEffect(() => {
+  inputRef.current.focus()
+})
 
 const handleChange = e => {
   setInput(e.target.value)
@@ -17,7 +23,7 @@ const handleSubmit = e => {
 }
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
-        <input type='text' placeholder='Add a todo' value={input} name='text' className='todo-input' onChange={handleChange} />
+        <input type='text' placeholder='Add a todo' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef} />
         <button className='todo-Button'>Add todo</button>
     </form>
   )
